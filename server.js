f4b6b968f3bf716d606883f4879b9f0aec465d76
kosmicbird <kosmicbird@gmail.com>
2018-01-13 00:01:36 -0600

    Add project page

--- server.js
+++ server.js
@@ -51,6 +51,11 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Project Page',
+    });
+});
 
 
 app.get('/bad', (req, res) => {
